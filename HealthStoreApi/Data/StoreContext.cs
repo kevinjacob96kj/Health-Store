4e0a92809527190c4691af17d919068c77c588d5
kevinjacob96kj <kevinjacob96kj@gmail.com>
2025-07-15 11:53:20 -0500

    commit 1

---
+++ HealthStoreApi/Data/StoreContext.cs
@@ -0,0 +1,10 @@
+﻿using HealthStoreApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthStoreApi.Data
+{
+    public class StoreContext(DbContextOptions options): DbContext(options)
+    {
+        public DbSet <Product> Products { get; set; }
+    }
+}
